3b1b20f60a8b625a92571ccd04a67d637fdca5fe
darrickyong <darrickyong@gmail.com>
2020-10-31 17:14:31 -0700

    Add app unit tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,11 +1,24 @@
 import React from "react";
+import ReactDOM from 'react-dom'
+import ShallowRenderer from 'react-test-renderer/shallow';
+import renderer from 'react-test-renderer';
 import { shallow } from "enzyme";
 import App from "./App";
 
 describe('My App', () => {
+  
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+  
+  it('renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
   it('should render correctly with no props', () => {
     const component = shallow(<App />);
     expect(component).toMatchSnapshot();
   });
-
 });
