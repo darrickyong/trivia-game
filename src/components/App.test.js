42fdf52e0a02d4c02821c2774fa45a4e1edf902e
darrickyong <darrickyong@gmail.com>
2020-10-31 17:10:53 -0700

    start unit testing

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,11 @@
-import render from "react-test-renderer";
+import React from "react";
+import { shallow } from "enzyme";
 import App from "./App";
-import Game from "./Game";
 
-test('Testing', () => {
-  const test = render.create(<App />)
-  
-  expect(true).toEqual(true);
-})
+describe('My App', () => {
+  it('should render correctly with no props', () => {
+    const component = shallow(<App />);
+    expect(component).toMatchSnapshot();
+  });
+
+});
