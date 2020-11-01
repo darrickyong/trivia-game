ed1d0abd81b78090b87aff564593af0d4efad3fd
darrickyong <darrickyong@gmail.com>
2020-10-31 22:58:14 -0700

    Add question unit tests

--- src/components/Game.test.js
+++ src/components/Game.test.js
@@ -1,20 +1,19 @@
 import React from "react";
-import ShallowRenderer from 'react-test-renderer/shallow';
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import Game from "./Game";
 import Question from "./Question";
 import defaultData from "../Apprentice_TandemFor400_Data.json";
 
 
 describe('The game', () => {
+  const game = shallow(<Game triviaData={defaultData} />);
+  const initialQuestion = game.find(Question);
   
   it('renders correctly with props', () => {
-    let game = shallow(<Game triviaData={defaultData} />);
     expect(game.is('.game')).toBe(true);
   })
 
   it('renders a question', () => {
-    let game = shallow(<Game triviaData={defaultData} />);
-    expect(game.find(Question).exists()).toBe(true);
+    expect(initialQuestion).toHaveLength(1);
   })
 })
--- src/components/Question.test.js
+++ src/components/Question.test.js
@@ -1,13 +1,23 @@
 import React from "react";
-import ReactDOM from 'react-dom'
-import ShallowRenderer from 'react-test-renderer/shallow';
-import renderer from 'react-test-renderer';
 import { shallow } from "enzyme";
 import Question from "./Question";
 
 describe('A question', () => {
-  
-  it('should have four choices', () => {
+  const currentQuestion = {
+    "question": "What was Tandem previous name?",
+    "incorrect": ["Tandem", "Burger Shack", "Extraordinary Humans"],
+    "correct": "Devmynd"
+  };
 
+  const choices = ["Tandem", "Burger Shack", "Extraordinary Humans", "Devmynd"];
+  const question = shallow(<Question currentQuestion={currentQuestion} choices={choices}/>)
+
+  it('should have four choices', () => {
+    expect(question.find('.question-choice')).toHaveLength(4);
   })
+
+  it('should have a next button', () => {
+    expect(question.find('.question-next')).toHaveLength(1);
+  })
+  
 })
